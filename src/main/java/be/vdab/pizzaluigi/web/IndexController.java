307a4df31bc2933cf3e68eeaed68c61afecbc054
Stijn Verholen <stijn.Verholen@vdabcampus.be>
2018-10-04 15:29:53 +0200

    Added a JSP

--- src/main/java/be/vdab/pizzaluigi/web/IndexController.java
+++ src/main/java/be/vdab/pizzaluigi/web/IndexController.java
@@ -1,6 +1,5 @@
 package be.vdab.pizzaluigi.web;
 
-import java.time.LocalTime;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,14 +11,6 @@ class IndexController {
 
 	@GetMapping
 	public final String index() {
-		int hour = LocalTime.now().getHour();
-		
-		if (hour < 12)
-			return "Good morning";
-		
-		if (hour < 18)
-			return "Good afternoon";
-		
-		return "Good evening";
+		return "/WEB-INF/JSP/index.jsp";
 	}
 }
